62f655ec61237f72ea5306cf4240c80c666fc4b3
gaurav4192 <srv.gaurav.100@gmail.com>
2026-03-17 16:15:50 +0530

    first push

---
+++ assets/projects.js
@@ -0,0 +1,148 @@
+const portfolioData = [
+  
+  {
+    id: "event-management",
+    category: "Event Management",
+    date: "November 2025",
+    client: "UAE Client",
+    title: "Event Management Platform",
+    website: "https://uaeiaa.org/members-meetings/",
+    nextProject: "",
+    role: "Flutter",
+    overview:
+      "Customized the Fluttter Frontend to develop IOS and Android App with REST API's for a mobile event management application. Provided a seamless experience for attendees and organizers by integrating booking, QR codes, and real-time attendance tracking.",
+    challenge:
+      "The client needed a digital solution to handle event registrations, generate attendee badges, manage sessions, and gather feedback efficiently, Attendence Managment etc.",
+    solution:
+      "Developed IOS and Android App with functionality and provide secure with REST API. Implemented QR-based user profiles, automated badge printing, session-wise attendance tracking, feedback submission, and push notifications using FCM.",
+    techStack: [
+      "DART",
+      "KOTLIN",
+	  "Swift",
+      "Custom Librasry",
+      "SQLLITE",
+      "REST API",
+      "FCM",
+    ],
+    image: [
+      "assets/img/portfolio/Project-1.png",
+     
+    ],
+    keyFeatures: [
+      "User-wise Event Booking",
+      "QR Code Profile Generation",
+      "Automated Badge Printing",
+      "Session-Wise Attendance Tracking",
+      "Feedback Submission",
+      "Event & Session Notifications",
+    ],
+  },
+  
+  
+    {
+    id: "lovpipo",
+    category: "Event Management",
+    date: "November 2024",
+    client: "Lovpipo",
+    title: "Lovpipo",
+    website: "https://lovpipo.io/",
+    nextProject: "",
+    role: "Flutter",
+    overview:
+      "Customized the Fluttter Frontend to develop IOS and Android App with REST API's for a mobile event       management application. Provided a seamless experience for attendees and organizers by integrating booking, QR codes, and real-time attendance tracking.",
+    challenge:
+      "The client needed a matrimony solution to handle event social login, live chat, manage sessions, matching life partner with firebase integration",
+    solution:
+      "Developed custom socket chat programming with live chat with match,sending and receive request to the match making and optimized REST APIs for mobile integration. Implemented QR-based user profiles for sending request and push notifications using FCM.",
+    techStack: [
+      "DART",
+      "KOTLIN",
+	  "Swift",
+      "Custom Library",
+      "SQLLITE",
+      "REST API",
+      "FCM",
+    ],
+    image: [
+      "assets/img/portfolio/lovpipo.webp",
+ 
+    ],
+    keyFeatures: [
+      "Socket Chat",
+      "Match Making",
+      "Search New Friend",
+      "Finding Particular Friend",
+      "Feedback Submission",
+      "Event & Session Notifications",
+    ],
+  },
+  
+   {
+    id: "GreenField School",
+    category: "School Management",
+    date: "November 2023",
+    client: "GreenField School",
+    title: "GreenField School",
+    website: "",
+    nextProject: "family-governance",
+    role: "backend",
+    overview:
+      "Customized the Fluttter Frontend to develop IOS and Android App with REST API's for a mobile event       management application. Provided a seamless experience for attendence of students,student profile details ,fees submission,teacher and student managment",
+    challenge:
+      "The client needed a seamless experience for attendence of students,student profile details ,fees submission,teacher and student managment",
+    solution:
+      "Developed seamless experience for attendence of students,student profile details ,fees submission,teacher and student managment, and push notifications using FCM.",
+    techStack: [
+      "Android",
+      "Kotlin",
+      "REST API",
+      "FCM",
+    ],
+    image: [
+      "assets/img/portfolio/greenfield.webp",
+     
+    ],
+    keyFeatures: [
+      "User-wise Event Booking",
+      "QR Code Profile Generation",
+      "Automated Badge Printing",
+      "Session-Wise Attendance Tracking",
+      "Feedback Submission",
+      "Event & Session Notifications",
+    ],
+  },
+  
+  {
+    id: "Spiritual Quotient",
+    category: "Spiritual",
+    date: "November 2023",
+    client: "Spiritual Quotient",
+    title: "Spiritual Quotient",
+    website: "https://play.google.com/store/apps/details?id=com.everynation.church",
+    nextProject: "family-governance",
+    role: "backend",
+    overview:
+      "Customized the Fluttter Frontend to develop IOS and Android App with REST API's for a mobile event       management application. Provided a seamless experience for attendence of students,student profile details ,fees submission,teacher and student managment",
+    challenge:
+      "The client needed a seamless experience for attendence of students,student profile details ,fees submission,teacher and student managment",
+    solution:
+      "Developed seamless experience for attendence of students,student profile details ,fees submission,teacher and student managment, and push notifications using FCM.",
+    techStack: [
+      "Android",
+      "Kotlin",
+      "REST API",
+      "FCM",
+    ],
+    image: [
+      "assets/img/portfolio/everynation.webp",
+     
+    ],
+    keyFeatures: [
+      "Socket Chat",
+      "QR Code Profile Generation",
+      "Session-Wise Attendance Tracking",
+      "Feedback Submission",
+      "Event & Session Notifications",
+    ],
+  },
+];
